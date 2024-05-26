0386dbd539aa38f7d6ebfe6a6dce97260fa7f8c7
AllenXeon07 <franciscoallenxeon@gmail.com>
2024-05-26 22:10:48 +0700

    add FBX

--- main.js
+++ main.js
@@ -1,116 +1,6 @@
 import * as THREE from "three";
-import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import { Player, PlayerController, ThirdPersonCamera } from "./player.js";
 
-class Player{
-
-    constructor(camera, controller, scene, speed){
-        this.camera = camera;
-        this.controller = controller;
-        this.scene = scene;
-        this.speed = speed;
-
-        this.camera.setup(new THREE.Vector3(0,0,0));
-
-        this.mesh = new THREE.Mesh(
-            new THREE.BoxGeometry(1,1,1),
-            new THREE.MeshPhongMaterial({color: 0xFF1111})
-        );
-        this.scene.add(this.mesh);
-        this.mesh.castShadow = true;
-        this.mesh.receiveShadow = true;
-    }
-
-    update(dt){
-        var direction = new THREE.Vector3(0,0,0);
-        if(this.controller.keys['forward']){
-            direction.x = 1;
-        }
-        if(this.controller.keys['backward']){
-            direction.x = -1;
-        }
-        if(this.controller.keys['left']){
-            direction.z = -1;
-        }
-        if(this.controller.keys['right']){
-            direction.z = 1;
-        }
-        
-        this.mesh.position.add(direction.multiplyScalar(dt*this.speed));
-        this.camera.setup(this.mesh.position);
-    }
-
-}
-
-class PlayerController{
-
-    constructor(){
-        this.keys = {
-            "forward": false,
-            "backward": false,
-            "left": false,
-            "right": false
-        }
-        document.addEventListener('keydown', (e) => this.onKeyDown(e), false);
-        document.addEventListener('keyup', (e) => this.onKeyUp(e), false);
-    }
-    onKeyDown(event){
-        switch(event.keyCode){
-            case "W".charCodeAt(0):
-            case "w".charCodeAt(0):
-                this.keys['forward'] = true;
-                break;
-            case "S".charCodeAt(0):
-            case "s".charCodeAt(0):
-                this.keys['backward'] = true;
-                break;
-            case "A".charCodeAt(0):
-            case "a".charCodeAt(0):
-                this.keys['left'] = true;
-                break;
-            case "D".charCodeAt(0):
-            case "d".charCodeAt(0):
-                this.keys['right'] = true;
-                break;
-        }
-    }
-    onKeyUp(event){
-        switch(event.keyCode){
-            case "W".charCodeAt(0):
-            case "w".charCodeAt(0):
-                this.keys['forward'] = false;
-                break;
-            case "S".charCodeAt(0):
-            case "s".charCodeAt(0):
-                this.keys['backward'] = false;
-                break;
-            case "A".charCodeAt(0):
-            case "a".charCodeAt(0):
-                this.keys['left'] = false;
-                break;
-            case "D".charCodeAt(0):
-            case "d".charCodeAt(0):
-                this.keys['right'] = false;
-                break;
-        }    
-    }
-
-}
-
-class ThirdPersonCamera{
-    constructor(camera, positionOffSet, targetOffSet){
-        this.camera = camera;
-        this.positionOffSet = positionOffSet;
-        this.targetOffSet = targetOffSet;
-    }
-    setup(target){
-        var temp = new THREE.Vector3(0,0,0);
-        temp.addVectors(target, this.positionOffSet);
-        this.camera.position.copy(temp);
-        temp = new THREE.Vector3(0,0,0);
-        temp.addVectors(target, this.targetOffSet);
-        this.camera.lookAt(temp);
-    }
-}
 
 class Main{
     static WindowResize(){
@@ -130,10 +20,6 @@ class Main{
         window.addEventListener('resize', () => {
             Main.WindowResize();
           }, false);
-
-        const controls = new OrbitControls(this.camera, this.renderer.domElement);
-        controls.target.set(0, 5, 0);
-        controls.update();
 
         //Plane
         var plane = new THREE.Mesh( new THREE.PlaneGeometry( 100, 100 ), new THREE.MeshPhongMaterial( { color: 0xcbcbcb } ) );
